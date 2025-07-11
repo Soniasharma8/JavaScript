b5d9cfd20afdc166af1b2ce5795aab810d2a7eb3
Sonia Sharma <Soniashar2021@gmail.com>
2025-07-11 19:00:58 +0530

    Update script.js

--- Variables/script.js
+++ Variables/script.js
@@ -1,5 +1,7 @@
 // Question 1: Declare a variable named myPathVariable and set its value to "Hello World!". Print this variable in console.log().
 
-var myPathVariable = "Hello World!"
+/*  var myPathVariable = "Hello World!"
 
-console.log(myPathVariable);
+console.log(myPathVariable);  */
+
+
