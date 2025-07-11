c9286c8ec9ba0d0d0679d7cdf3f43f87181e3be4
Sonia Sharma <Soniashar2021@gmail.com>
2025-07-11 22:57:37 +0530

    Update script.js

--- Variables/script.js
+++ Variables/script.js
@@ -4,4 +4,67 @@
 
 console.log(myPathVariable);  */
 
+//Question 2: Create a variable myAge and assign its value to your current age number. Then store a separate variable nextYearAge by adding 1 to the value of myAge. Print both variables in the console.
+
+/* 
+var myAge = 23;
+var nextYearAge = myAge + 1;
+
+console.log(myAge);
+console.log(nextYearAge);
+*/
+
+
+// Question 3: firstName naam ka ek variable banao aur usmein apna pehla naam string mein assign karo. Phir lastName naam ka ek aur variable banao aur usmein apna aakhri naam assign karo. Ab fullName naam ka ek teesra variable banao aur usmein firstName aur lastName ko jodkar assign karo. fullName ko print karo. */
+
+/*
+let firstName = "Sonia";
+let lastName = "Sharma";
+let fullName = firstName + " " + lastName;
+console.log(fullName); 
+
+*/
+// Question 4: Ek variable isStudent banao aur uski value true assign karo (boolean type). Phir ek aur variable isEmployed banao aur uski value false assign karo. Dono ko print karo.
+
+/*
+let isStudent = "true";
+let isEmployed = "false";
+console.log(isStudent);
+console.log(isEmployed); 
+*/
+
+let price = 25.50;
+
+let quantity = 2;
+let totalCost = price * quantity;
+
+//console.log(totalCost);
+
+
+
+/*
+count = 0;
+count = count += 5;
+console.log(count);
+
+*/
+
+
+//Question: Ek variable score banao aur uski value 100 rakho. Score ko 20 se kam karo. Phir updated score print karo.
+/*
+let score = 100;    
+score = score - 20;
+console.log(score);
+*/
+
+
+let message = "Hello";
+message = message += " world";
+console.log(message);
+
+
+
+
+
+
 
