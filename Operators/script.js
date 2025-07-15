311a463382a5e1ff8c1ffd1ea8f5fa60bd02356d
Sonia Sharma <Soniashar2021@gmail.com>
2025-07-15 19:35:44 +0530

    Create script.js

---
+++ Operators/script.js
@@ -0,0 +1,39 @@
+// Question 1: The value of num1 is 10 and the value of num2 is 5. Create a new variable sum and store the total of num1 and num2 in it. Print the sum in the console.
+
+// let num1 = 10;
+// let num2 = 5;
+// let sum = num1 + num2;
+// console.log(sum);
+
+// Question 2: The value of appleCount is 20 and the value of eatenApples is 7. Create a variable remainingApples and store the count of remaining apples in it. Print remainingApples in the console.
+
+// let appleCount = 20;
+// let eatenApples = 7;
+// let remainingApples = appleCount - eatenApples;
+// console.log(remainingApples);
+
+// Question 3: Multiplication: The value of pricePerItem is 15 and the value of quantity is 4. Create a variable totalCost and store the total cost in it. Print totalCost in the console.
+
+// let pricePerItem = 15;
+// let quantity =  4;
+// let totalCost = pricePerItem * quantity;
+// console.log(totalCost);
+
+// Question 4: Division: The value of totalStudents is 50 and the value of groups is 10. Create a variable studentsPerGroup and calculate how many students will be there in each group. Print studentsPerGroup in the console.
+
+// let totalStudents = 50;
+// let groups = 10;
+// let studentsPerGroup = totalStudents / groups;
+// console.log(studentsPerGroup);
+
+//Question 5: Modulus (Remainder): The value of total Chocolates is 23 and the value of students is 5. If every student gets equal number of chocolates, how many chocolates will be left? Create a variable remainingChocolates and store the remainder in it. Print remainingChocolates in the console.
+
+// let totalchocolates = 23;
+// let students = 7;
+// let remainingChocolates = totalchocolates % students;
+// console.log(remainingChocolates);
+
+
+let score = 100;
+score = score ++;
+console.log(score); // Output: 101
