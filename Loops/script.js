59194dc1ec1bedc4621cd91c81d8caf81df4b55b
Sonia Sharma <Soniashar2021@gmail.com>
2025-07-20 22:42:09 +0530

    Create script.js

---
+++ Loops/script.js
@@ -0,0 +1,15 @@
+/*
+// Question 1: Print Numbers 1 to 10
+
+for (let i = 1; i <11; i++) {
+    console.log(i);
+}
+
+// Question 2 :
+
+for (let i = 1; i < 21; i++) {
+    if(i % 2 === 0) {
+        console.log(i);   
+    } }
+
+    */
