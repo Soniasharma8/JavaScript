3da85d8481b281f38b6a7d3cc7734bc2e2e76d2f
Sonia Sharma <Soniashar2021@gmail.com>
2025-07-21 21:51:42 +0530

    Update script.js

--- Loops/script.js
+++ Loops/script.js
@@ -1,10 +1,9 @@
-/*
+ /*
 // Question 1: Print Numbers 1 to 10
 
 for (let i = 1; i <11; i++) {
     console.log(i);
 }
-
 // Question 2 :
 
 for (let i = 1; i < 21; i++) {
@@ -12,4 +11,39 @@ for (let i = 1; i < 21; i++) {
         console.log(i);   
     } }
 
-    */
+   
+
+  // Question 3 :  
+
+  for (let i = 1; i < 16; i++) {
+    if ( i % 2 !== 0) {
+        console.log(i);
+    }
+    }
+  
+// Question 4:  
+
+let sum = 0;
+for (let i = 1; i <= 5; i++) {
+    sum += i;
+}
+console.log("Sum of 1 to 5:", sum);
+ 
+// Question 5: 
+
+let factorial = 1;
+for (let i = 1; i <= 5; i++) { 
+    factorial *= i; 
+}
+console.log("Factorial of 5:", factorial);
+
+
+// Question 6: 
+for (let i = 1; i <= 10; i++) {
+    console.log(7 * i);
+    
+}
+
+*/
+
+// Question 7: 
